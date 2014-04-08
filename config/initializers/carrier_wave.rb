46381534a60d7792e407b372e502e138e149f226
Steve Quatrani <spq2461@gmail.com>
2014-04-08 18:33:01 -0400

    uploader fixes

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,15 +1,16 @@
 CarrierWave.configure do |config|
-  if Rails.env.staging? || Rails.env.production?
-    config.storage = :fog
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
       :aws_access_key_id      => ENV['S3_KEY'],                        # required
       :aws_secret_access_key  => ENV['S3_SECRET'],                        # required
       :region                 => ENV['S3_REGION'],                  # optional, defaults to 'us-east-1'
     }
-    config.fog_directory  = 'myflixq'                     # required
-  else
-    config.storage = :file
-    config.enable_processing = false
-  end
+    config.fog_directory    = ENV['S3_BUCKET_NAME'] # required
+
+    if Rails.env.test? || Rails.env.development?                      
+      config.storage = :file
+      config.enable_processing = false
+    else
+        config.storage = :fog
+    end
 end
