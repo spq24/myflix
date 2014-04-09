c0b5618135980641b8c29e6367f4247d09583d6c
spq24 <spq2461@gmail.com>
2014-04-09 01:54:50 -0400

    delete carrier_wave.rb

--- config/initializers/carrier_wave.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-    config.fog_credentials = {
-      :provider               => 'AWS',                        # required
-      :aws_access_key_id      => 'AKIAJWPCMYQ4Q3OEB7QQ',                        # required
-      :aws_secret_access_key  => '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',                       
-    }
-    config.fog_directory   = 'myflixq' # required
-    config.fog_public = false
-end
