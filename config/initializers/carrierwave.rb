68e602e7969024de6d7c2ce3263ce3862d554ae9
Steve Quatrani <spq2461@gmail.com>
2014-04-09 01:51:01 -0400

    hiding s3 keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider               => 'AWS',                        # required
-      :aws_access_key_id      => 'AKIAI7VMQRM4JCORH35A',                        # required
-      :aws_secret_access_key  => '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',                        # required              # optional, defaults to 'us-east-1'
+      :aws_access_key_id      => ENV['S3_KEY'],                        # required
+      :aws_secret_access_key  => ENV['S3_SECRET'],                        # required              # optional, defaults to 'us-east-1'
     }
-    config.fog_directory   = 'myflixq' # required
+    config.fog_directory   = ENV['S3_BUCKET_NAME'] # required
     config.fog_public = false
 end
