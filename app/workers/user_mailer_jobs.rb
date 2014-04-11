9e04a48e9b6c223c667096e7217c25f27a49f055
Steve Quatrani <spq2461@gmail.com>
2014-04-11 03:22:55 -0400

    let us see

--- app/workers/user_mailer_jobs.rb
+++
@@ -1,9 +0,0 @@
-class UserMailerJobs
-	include Sidekiq::Worker
-
-	def perform(invitation_token)
-	   invitation = Invitation.find_by token: 'invitation_token'
-	   UserMailer.send_invitation_email(invitation).deliver
-	end
-
-end
