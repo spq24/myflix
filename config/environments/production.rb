fa09d6a8fac6a1b76ac1033e770fea29648111f1
Steve Quatrani <spq2461@gmail.com>
2014-04-11 03:13:44 -0400

    aws yelled at me

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,24 @@ Myflix::Application.configure do
   }
   ActionMailer::Base.delivery_method = :smtp
 
+  CarrierWave.configure do |config|
+    if Rails.env.production? || Rails.env.development?
+        config.fog_credentials = {
+          :provider               => 'AWS',
+          :aws_access_key_id      => ENV['S3_KEY'],
+          :aws_secret_access_key  => ENV['S3_SECRET'],
+          :region                 => 'us-east-1'                        # required              # optional, defaults to 'us-east-1'
+        }
+        config.fog_directory   = ENV['S3_BUCKET_NAME'] # required
+        config.storage = :fog
+        config.cache_dir = 'carrierwave'
+        config.fog_public = false
+    else
+      config.storage = :file
+      config.enable_processing = false
+    end
+  end
+
   config.cache_classes = true
   config.eager_load = true
   config.consider_all_requests_local       = false
