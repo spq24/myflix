8201830df13b74035d9dad1de46cd5ed1a5121ba
Steve Quatrani <spq2461@gmail.com>
2014-04-11 03:30:55 -0400

    figuring this ish out

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,6 +10,8 @@ CarrierWave.configure do |config|
 	    config.storage = :fog
 	    config.cache_dir = 'carrierwave'
 	    config.fog_public = false
+	    storage = Fog::Storage.new(:provider => 'AWS', :aws_access_key_id => 'ACCESS_KEY_ID', :aws_secret_access_key => 'SECRET_ACCESS_KEY')
+		compute = Fog::Compute.new(:provider => 'AWS', :aws_access_key_id => 'ACCESS_KEY_ID', :aws_secret_access_key => 'SECRET_ACCESS_KEY')
 	else
 		config.storage = :file
 		config.enable_processing = false
