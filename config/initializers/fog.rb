56295d337b3bc7ae2a6cc30737e862ae88c5bf54
Steve Quatrani <spq2461@gmail.com>
2014-04-13 02:24:09 -0400

    working?

--- config/initializers/fog.rb
+++
@@ -1,7 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => Rails.configuration.s3_access_key_id,
-    :aws_secret_access_key  => Rails.configuration.s3_secret_access_key,
-  }
-end
