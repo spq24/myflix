58d394f54f28f49ae8c24611352e17871fbadd46
Steve Quatrani <spq2461@gmail.com>
2014-04-13 01:41:11 -0400

    carrierwave-aws gem

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,17 +1,12 @@
 CarrierWave.configure do |config|
 	if Rails.env.production? || Rails.env.development?
-	    config.fog_credentials = {
-	      :provider               => 'AWS',
-	      :aws_access_key_id      => ENV['S3_KEY'],
-	      :aws_secret_access_key  => ENV['S3_SECRET'],
-	      :region                 => 'us-east-1'                        # required              # optional, defaults to 'us-east-1'
+	    config.aws_credentials = {
+   		  access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
+    	  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
 	    }
-	    config.fog_directory   = ENV['S3_BUCKET_NAME'] # required
-	    config.storage = :fog
-	    config.cache_dir = 'carrierwave'
-	    config.fog_public = false
-	    storage = Fog::Storage.new(:provider => 'AWS', :aws_access_key_id => 'ACCESS_KEY_ID', :aws_secret_access_key => 'SECRET_ACCESS_KEY')
-		compute = Fog::Compute.new(:provider => 'AWS', :aws_access_key_id => 'ACCESS_KEY_ID', :aws_secret_access_key => 'SECRET_ACCESS_KEY')
+	    config.aws_bucket  = ENV['S3_BUCKET_NAME'] # required
+	    config.storage = :aws
+
 	else
 		config.storage = :file
 		config.enable_processing = false
