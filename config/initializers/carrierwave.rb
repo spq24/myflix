18a83cba1e8dfa4233aee587c3c2319c91e5232b
Steve Quatrani <spq2461@gmail.com>
2014-04-13 20:31:17 -0400

    carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,7 +10,6 @@ CarrierWave.configure do |config|
 	    config.storage = :fog
 	    config.fog_public = false
 	    config.cache_dir = "#{Rails.root}/tmp/uploads"
-	    config.fog_host = "#{ENV['S3_ASSET_URL']}/#{ENV['S3_BUCKET_NAME']}"  
 	else
 		config.storage = :file
 		config.enable_processing = false
