e925d349e130a1aa4cfda6a6dc86bb7ad3cc721a
Steve Quatrani <spq2461@gmail.com>
2014-05-12 03:02:33 -0400

    user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,7 +51,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :full_name)
+    params.require(:user).permit(:email, :password, :full_name, :admin)
   end
 
   def handle_invitation
